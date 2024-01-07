8adea2f7d76b6d9e43940962aea855a795c37bc8
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-01-08 01:16:26 +0500

    update things

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,11 +1,34 @@
+import 'package:breast_onco/themes/colors.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatelessWidget {
-  const AccountScreen({super.key});
+  const AccountScreen({Key? key}) : super(key: key);
   static const routeName = '/account';
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Text(
+                'User Account',
+                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: kSecondarySwatchColor,
+                  fontWeight: FontWeight.bold,
+                )
+              ),
+         
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
